983196a1305441acf02c16924bc430560863be8e
Candice Smith MacKenzie <Mahony8@aol.com>
2022-03-27 17:08:15 -0400

    card change

--- src/components/knights/KnightCards.jsx
+++ src/components/knights/KnightCards.jsx
@@ -5,11 +5,11 @@ const KnightCards = ({ knight }) => {
 
     return (
       <div className='card' style={{textAlign: "center"}}>
-          <h2 style={{fontFamily: "fantasy"}}>{name}</h2>
+          <h2 style={{fontFamily: "fantasy", color: "darkgrey"}}>{name}</h2>
           <img src={image} alt={name} height={250} width={400} quality={.05}/>
-          <p style={{fontFamily: "fantasy"}}>Class: {type} Powers: {powers}</p>
-        <button style={{fontFamily: "fantasy", backgroundColor: "darkgrey"}}>Add to Army</button>
-        <button style={{fontFamily: "fantasy", backgroundColor: "darkgrey"}}>Favorite</button>
+          <p style={{fontFamily: "fantasy", color: "darkgrey"}}>Class: {type} Powers: {powers}</p>
+        <button style={{fontFamily: "fantasy", color: "darkgrey", backgroundColor: "rgb(59, 55, 55)"}}>Add to Army</button>
+        <button style={{fontFamily: "fantasy", color: "darkgray", backgroundColor: "rgb(59, 55, 55)"}}>Favorite</button>
       </div>
     )
   }
