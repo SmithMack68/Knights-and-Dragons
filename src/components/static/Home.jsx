1e5d73033be3b627ee3a669ae15f4bb474bcd4e9
Candice Smith MacKenzie <Mahony8@aol.com>
2022-03-29 00:25:18 -0400

    changed welcome

--- src/components/static/Home.jsx
+++ src/components/static/Home.jsx
@@ -7,7 +7,8 @@ const Home = () => {
     <div>
         <h1 style={{ textAlign: "center", fontFamily: "fantasy", fontSize: 65, color: "whitesmoke"}}>Welcome to Knights & Dragons</h1>
         <br></br>
-        <p style={{ textAlign: "center", fontFamily: "fantasy", fontSize: 40, color: "whitesmoke"}}>Choose your </p>
+        <p style={{ textAlign: "center", fontFamily: "fantasy", fontSize: 40, color: "whitesmoke"}}>Choose who will go into battle with you.
+         <br></br>But choose wisely. <br></br>Not all Dragons have the same powers or abilities.</p>
     </div>
   )
 }
