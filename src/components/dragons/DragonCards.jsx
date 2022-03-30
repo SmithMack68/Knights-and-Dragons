d6a42c0ce1b936448a61fd3fb5d32ed899c9fe54
Candice Smith MacKenzie <Mahony8@aol.com>
2022-03-30 00:40:20 -0400

    added Powers to db.json

--- src/components/dragons/DragonCards.jsx
+++ src/components/dragons/DragonCards.jsx
@@ -17,7 +17,7 @@ const DragonCards = ({ dragon }) => {
     <div className='card' style={{textAlign: "center"}}>
         <h2 style={{fontFamily: "fantasy", color: "orangered"}}>{name}</h2>
         <img src={image} alt={name} height={300} width={400} quality={.05}/>
-        <p style={{fontFamily: "fantasy", color: "orangered"}}>Class: {type} Powers: {powers}</p>
+        <p style={{fontFamily: "fantasy", color: "orangered"}}>Class: {type} <br></br>Powers: {powers}</p>
       <button onClick={addToArmy} style={{fontFamily: "fantasy", color: "orangered", backgroundColor: "rgb(59, 55, 55)"}}>{inArmy ? "Add to Army" : "Remove from Army"}</button>
       <button onClick={handleFavoriteClick} style={{fontFamily: "fantasy", color: "orangered", backgroundColor: "rgb(59, 55, 55)"}}>{favorite ? "Favorite": " 🔥 " }</button>
     </div>
--- src/components/knights/KnightCards.jsx
+++ src/components/knights/KnightCards.jsx
@@ -16,7 +16,7 @@ const KnightCards = ({ knight }) => {
       <div className='card' style={{textAlign: "center"}}>
           <h2 style={{fontFamily: "fantasy", color: "darkgrey"}}>{name}</h2>
           <img src={image} alt={name} height={250} width={400} quality={.05}/>
-          <p style={{fontFamily: "fantasy", color: "darkgrey"}}>Class: {type} Powers: {powers}</p>
+          <p style={{fontFamily: "fantasy", color: "darkgrey"}}>Class: {type} <br></br>Powers: {powers}</p>
         <button onClick={addToArmy} style={{fontFamily: "fantasy", color: "darkgrey", backgroundColor: "rgb(59, 55, 55)"}}>{inArmy ? "Add to Army" : "Remove from Army"}</button>
         <button onClick={handleFavoriteClick} style={{fontFamily: "fantasy", color: "darkgrey", backgroundColor: "rgb(59, 55, 55)"}}>{favorite ? "Favorite" : "🗡"  }</button>
       </div>
